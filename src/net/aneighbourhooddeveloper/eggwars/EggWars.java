22b0f91990cd3a9b8c36fd6be442a30ee28999ca
ANeighbourhoodDeveloper <89845111+ANeighbourhoodDeveloper@users.noreply.github.com>
2021-08-31 13:32:43 +0200

    Added files via upload

---
+++ src/net/aneighbourhooddeveloper/eggwars/EggWars.java
@@ -0,0 +1,52 @@
+package net.aneighbourhooddeveloper.eggwars;
+
+import net.aneighbourhooddeveloper.eggwars.events.BlockEvents;
+import net.aneighbourhooddeveloper.eggwars.events.EggBreakEvent;
+import net.aneighbourhooddeveloper.eggwars.events.GeneratorEvents;
+import net.aneighbourhooddeveloper.eggwars.events.InventoryEvents;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EggWars extends JavaPlugin {
+
+	TeamManager tm;
+	InventoryManager im;
+	GeneratorManager gm;
+	BlockManager bm;
+
+	public void onEnable() {
+		tm = new TeamManager();
+		im = new InventoryManager();
+		gm = new GeneratorManager(this);
+		bm = new BlockManager();
+		Bukkit.getPluginManager().registerEvents(new EggBreakEvent(this), this);
+		Bukkit.getPluginManager().registerEvents(new GeneratorEvents(this), this);
+		Bukkit.getPluginManager().registerEvents(new InventoryEvents(this), this);
+		Bukkit.getPluginManager().registerEvents(new BlockEvents(this), this);
+
+		gm.addGenerator("diamond", new Location(Bukkit.getWorld("world"), 0, 110, 11),2,5, 25, Material.DIAMOND);
+		gm.addGenerator("gold", new Location(Bukkit.getWorld("world"), -14, 110, 15),3,13, 25, Material.DIAMOND);
+		gm.addGenerator("iron", new Location(Bukkit.getWorld("world"), 15, 110, 13),3,27, 50, Material.GOLD_INGOT);
+
+		gm.startGenerators();
+	}
+
+	public TeamManager getTeamManager() {
+		return tm;
+	}
+
+	public InventoryManager getInventoryManager() {
+		return im;
+	}
+
+	public GeneratorManager getGeneratorManager() {
+		return gm;
+	}
+
+	public BlockManager getBlockManager() {
+		return bm;
+	}
+
+}
